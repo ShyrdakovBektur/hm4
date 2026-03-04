1e11ccd653c03942f70d83bd5457978aa63beb7a
Shyrdakov Bektur <123831402+ShyrdakovBektur@users.noreply.github.com>
2026-03-04 14:27:43 +0600

    first commit

---
+++ lesson4.js
@@ -0,0 +1,58 @@
+
+
+
+//1
+// let name = prompt("Привет как тебя зовут?");
+// alert(name + " О а меня Бектур");
+// let from = prompt(name + "Ты откуда?");
+// if(form == "American"){
+//     alert("O bro I am learning English how are you?");
+// }else if(from == "Uzbekistan") {
+// alert(from + "E ukam kolisan gaprashamis go");
+// }else if (from == "Frace") {
+//     alert( forom + " bonjur");
+// }else {
+//     alert(акщь + "Извините не занаю такого языка");
+// }
+
+//2
+// let symbol = prompt("Символдорду териниз");
+// if (symbol[0] =="а") {
+//     alert(symbol[0] + " oa туура");
+// }else {
+//     alert(symbol[0] +  " жок туура эмес");
+// }
+
+//3
+// let password = prompt("Введите пароль");
+// if(password[0] === "1" && password[1] === "2" && password[2] === "3" ) {
+//     alert("туура ураа");
+// }else {
+//     alert("туура эмес");
+// }
+
+//1
+// let num = prompt("Введите число");
+// if (num % 2 == 0) {
+//     alert("жуп сан");
+// }else {
+//     alert("так сан");
+// }
+
+
+
+//2
+// let text = prompt("Введите месяц"); 
+// if (text === "Январь" || text==="Декабрь" || text==="Февраль") {
+//     alert("Зима");
+// }else if (text === "Юнь" || text==="Юль" ||text=== "Август") {
+//     alert("Лето");
+// }else if (text === "Сентябрь" || text==="Октябрь" ||text=== "Ноябрь") {
+//     alert("Осень");
+// }else if (text === "Март" || text==="Апрель" || text==="Май") {
+//     alert("Весна");
+// }
+
+
+//3
+
